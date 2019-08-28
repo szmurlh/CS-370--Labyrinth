3a516d096db69875139c65a59fccd171a6967e3c
bloomja <bloomja@sunyit.edu>
2019-08-28 18:44:32 -0400

    Added sample images for tiles

--- workspace/Labyrinth/src/Driver.java
+++ workspace/Labyrinth/src/Driver.java
@@ -1,34 +1,113 @@
+import org.lwjgl.*;
+import org.lwjgl.glfw.*;
+import org.lwjgl.opengl.*;
+import org.lwjgl.system.*;
+
+import java.nio.*;
+
+import static org.lwjgl.glfw.Callbacks.*;
 import static org.lwjgl.glfw.GLFW.*;
-import org.lwjgl.glfw.GLFWVidMode;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryStack.*;
+import static org.lwjgl.system.MemoryUtil.*;
 
 public class Driver {
 	
 	private static int WINDOW_WIDTH = 640;
 	private static int WINDOW_HEIGHT = 480;
-	
-	public static void main(String[] args) {
-		
-		if (!glfwInit()) {
-			throw new IllegalStateException("Failed to initialize GLFW! ");
+
+	// The window handle
+	private long window;
+
+	public void run() {
+		System.out.println("Welcome to Labyrinth!");
+
+		init();
+		loop();
+
+		// Free the window callbacks and destroy the window
+		glfwFreeCallbacks(window);
+		glfwDestroyWindow(window);
+
+		// Terminate GLFW and free the error callback
+		glfwTerminate();
+		glfwSetErrorCallback(null).free();
+	}
+
+	private void init() {
+		// Setup an error callback. The default implementation
+		// will print the error message in System.err.
+		GLFWErrorCallback.createPrint(System.err).set();
+
+		// Initialize GLFW. Most GLFW functions will not work before doing this.
+		if ( !glfwInit() )
+			throw new IllegalStateException("Unable to initialize GLFW");
+
+		// Configure GLFW
+		glfwDefaultWindowHints(); // optional, the current window hints are already the default
+		glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // the window will stay hidden after creation
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
+
+		// Create the window
+		window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "CS370 - Labyrinth", NULL, NULL);
+		if ( window == NULL ) {
+			throw new RuntimeException("Failed to create the GLFW window");
 		}
-		
-		glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
-		long window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "CS 370 - Labyrinth", 0, 0);
-		if (window == 0) {
-			throw new IllegalStateException("Failed to create window! ");
-		}
-		
-		GLFWVidMode videoMode = glfwGetVideoMode(glfwGetPrimaryMonitor());
-		glfwSetWindowPos(window, (videoMode.width() - WINDOW_WIDTH)/2, (videoMode.height() - WINDOW_HEIGHT)/2);
-		
+
+		// Setup a key callback. It will be called every time a key is pressed, repeated or released.
+		glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
+			if ( key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE )
+				glfwSetWindowShouldClose(window, true); // We will detect this in the rendering loop
+			if ( key == GLFW_KEY_ENTER && action == GLFW_RELEASE ) {
+				glClearColor((float)Math.random(), (float)Math.random(), (float)Math.random(), 0.0f);
+			}
+		});
+
+		// Get the resolution of the primary monitor
+		GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+		// Center the window
+		glfwSetWindowPos(
+			window,
+			(vidmode.width() - WINDOW_WIDTH) / 2,
+			(vidmode.height() - WINDOW_HEIGHT) / 2
+		);
+
+		// Make the OpenGL context current
+		glfwMakeContextCurrent(window);
+		// Enable v-sync
+		glfwSwapInterval(1);
+
+		// Make the window visible
 		glfwShowWindow(window);
-		
-		while (!glfwWindowShouldClose(window)) {
+	}
+
+	private void loop() {
+		// This line is critical for LWJGL's interoperation with GLFW's
+		// OpenGL context, or any context that is managed externally.
+		// LWJGL detects the context that is current in the current thread,
+		// creates the GLCapabilities instance and makes the OpenGL
+		// bindings available for use.
+		GL.createCapabilities();
+
+		// Set the clear color
+		glClearColor((float)Math.random(), (float)Math.random(), (float)Math.random(), 0.0f);
+
+		// Run the rendering loop until the user has attempted to close
+		// the window or has pressed the ESCAPE key.
+		while ( !glfwWindowShouldClose(window) ) {
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
+
+			glfwSwapBuffers(window); // swap the color buffers
+
+			// Poll for window events. The key callback above will only be
+			// invoked during this call.
 			glfwPollEvents();
 		}
-		
-		glfwTerminate();
-		
+	}
+
+	public static void main(String[] args) {
+		new Driver().run();
 	}
 	
 }
