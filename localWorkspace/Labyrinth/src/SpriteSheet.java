5746a61397540fab9e37962051a5a0bfa24813ac
bloomja <bloomja@sunyit.edu>
2019-09-03 10:49:56 -0400

    Added the complete Java files to display the full 7x7 board. No movement functionality such as rotating and sliding. The board WILL be randomized every time though excluding the pieces that should remain stationary each game. Since there is no rotation, some of the stationary pieces are oriented wrong.

---
+++ localWorkspace/Labyrinth/src/SpriteSheet.java
@@ -0,0 +1,20 @@
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
+import java.awt.image.BufferedImage;
+
+public class SpriteSheet {
+	
+	private BufferedImage image;
+	
+	public SpriteSheet (BufferedImage ss) {
+		this.image = ss;
+	}
+	
+	// This method will pick a sprite from the
+	// spritesheet based on its x and y position.
+	public BufferedImage getImage(int x, int y, int width, int height) {
+		BufferedImage img = image.getSubimage(width*x, height*y, width-1, height-1);
+		return img;
+	}
+	
+}
